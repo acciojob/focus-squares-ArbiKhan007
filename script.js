4118d32df3cf4e983e262cfc2cf7430883ac1719
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-11-23 15:10:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const squares=document.querySelectorAll(".square");
+
+squares.forEach(square=>{
+	square.addEventListener("mouseover", (e)=>{
+		const targetId=e.currentTarget.id;
+
+		squares.forEach(s=>{
+			if(s.id!==targetId){
+				s.classList.add("square_coffee");
+				s.classList.remove("square");
+			}
+		})
+	});
+
+	square.addEventListener("mouseout", (e)=>{
+		squares.forEach(s=>{
+			s.classList.remove("square_coffee");
+			s.classList.add("square");
+		});
+	})
+});
